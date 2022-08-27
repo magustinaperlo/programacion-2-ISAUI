0ca9fd865a2b8f420d33ba6206be18c0d6e31ee2
GracielaYQP <96759881+GracielaYQP@users.noreply.github.com>
2022-08-27 15:27:35 -0300

    Add files via upload

---
+++ ej1listas.py
@@ -0,0 +1,8 @@
+#Escribir un programa que almacene las asignaturas de un curso (por
+#ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la
+#muestre por pantalla.
+
+asignaturas=["Matematicas","Fisica","Quimica","Historia", "Lengua" ]
+print (asignaturas)   #asi escribe las asignaturas en una fila
+for x in asignaturas:   #asi escribe las asignaturas en columna
+    print(asignaturas)
